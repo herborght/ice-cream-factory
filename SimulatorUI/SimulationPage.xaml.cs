aff99e0c1fb9dc496d52514f3573ee2fb4b236e6
joakim korhonen <JK1995@hotmail.se>
2021-12-04 00:03:32 +0100

    Added some tanks and implemented some valves and their connections

--- SimulatorUI/SimulationPage.xaml.cs
+++ SimulatorUI/SimulationPage.xaml.cs
@@ -22,6 +22,8 @@ namespace SimulatorUI
         List<TankModule> tankList;
         List<Rectangle> barList;
         List<TextBlock> textBlocks;
+        List<KeyValuePair<string, KeyValuePair<int, Point>>> pointList;
+        List<Ellipse> connectedValves;
         public SimulationPage(List<TankModule> list)
         {
             tankList = list;
@@ -36,14 +38,18 @@ namespace SimulatorUI
             int height = 200;
             int time = 0;
             int fromTop = 20;
+            int rows = 0;
             barList = new List<Rectangle>();
             textBlocks = new List<TextBlock>();
+            pointList = new List<KeyValuePair<string, KeyValuePair<int, Point>>>();
+            connectedValves = new List<Ellipse>();
             foreach (TankModule tank in tankList)
             {
                 if (time == 3)
                 {
-                    fromTop += 225;
+                    fromTop += 300;
                     time = 0;
+                    rows++;
                 }
 
                 Rectangle rectangle = new Rectangle();
@@ -56,7 +62,6 @@ namespace SimulatorUI
                 Canvas.SetTop(rectangle, fromTop);
                 rectangle.StrokeThickness = 2;
                 rectangle.Stroke = Brushes.Black;
-               
 
                 TextBlock textBlock = new TextBlock();
                 textBlock.Width = 250;
@@ -81,11 +86,87 @@ namespace SimulatorUI
                 other.Stroke = Brushes.Black;
                 barList.Add(other);
 
+                Point point = new Point();
+                point.X = time * 190 + 37.5;
+                point.Y = fromTop + height;
+                KeyValuePair<string, KeyValuePair<int, Point>> keyValuePair = new KeyValuePair<string, KeyValuePair<int, Point>>(tank.Name, new KeyValuePair<int, Point>(rows, point));
+
+                pointList.Add(keyValuePair);
+
                 canvas.Children.Add(rectangle);
                 canvas.Children.Add(other);
                 canvas.Children.Add(textBlock);
 
                 time++;
+            }
+            int[] times = new int[rows + 1]; //Used to increment the length of which the lines are apart from eachother
+            foreach(TankModule tank in tankList)
+            {
+                foreach(TankModule connected in tank.InFlowTanks) //This is a bit backward initial is the destination of the connection while target is the source
+                {
+                    KeyValuePair<int, Point> initialPair = pointList.Find(x => x.Key == tank.Name).Value;
+                    KeyValuePair<int, Point> targetPair = pointList.Find(x => x.Key == connected.Name).Value;
+                    int initialRow = initialPair.Key;
+                    times[initialRow]++;
+                    int targetRow = targetPair.Key;
+                    if(initialRow != targetRow)
+                    {
+                        times[targetRow]++;
+                    }
+                    Polyline line = new Polyline();
+                    line.Stroke = Brushes.Black;
+                    PointCollection points = new PointCollection();
+                    Point initial = initialPair.Value;
+                    Point target = targetPair.Value;
+                    points.Add(initial);
+                    Ellipse ellipse = new Ellipse();
+                    ellipse.Width = 10;
+                    ellipse.Height = 10;
+                    ellipse.Fill = Brushes.Black;
+                    ellipse.StrokeThickness = 2;
+                    ellipse.Stroke = Brushes.Black;
+                    ellipse.Uid = tank.Name + "_" + connected.Name;
+                    connectedValves.Add(ellipse);
+
+                    if (initial.Y == target.Y)
+                    {
+                        Point first = new Point();
+                        first.Y = initial.Y + times[initialRow] * 10;
+                        first.X = initial.X;
+                        Point second = new Point();
+                        second.Y = first.Y;
+                        second.X = target.X;
+                        points.Add(first);
+                        points.Add(second);                    
+                        Canvas.SetLeft(ellipse, first.X + (second.X - first.X) / 2);
+                        Canvas.SetTop(ellipse, first.Y - 5);
+                    }
+                    else
+                    {
+                        Point first = new Point();
+                        first.Y = initial.Y + times[initialRow] * 10;
+                        first.X = initial.X;
+                        Point second = new Point();
+                        second.Y = first.Y;
+                        second.X = first.X + 75;
+                        Point third = new Point();
+                        third.Y = target.Y + times[targetRow] * 10;
+                        third.X = second.X;
+                        Point fourth = new Point();
+                        fourth.Y = target.Y + times[targetRow] * 10;
+                        fourth.X = target.X;
+                        points.Add(first);
+                        points.Add(second);
+                        points.Add(third);
+                        points.Add(fourth);
+                        Canvas.SetLeft(ellipse, third.X - 5);
+                        Canvas.SetTop(ellipse, second.Y + (third.Y - second.Y) / 2);
+                    }
+                    points.Add(target);
+                    line.Points = points;
+                    canvas.Children.Add(line);
+                    canvas.Children.Add(ellipse);
+                }
             }
         }
 
@@ -107,14 +188,55 @@ namespace SimulatorUI
                         r.Dispatcher.Invoke(() => { r.Height = 200 - 200 * tankList.Find(x => x.Name == r.Uid).LevelPercentage / 100; });
                     }
                 }
-                foreach (TextBlock textBlock in textBlocks)
+                foreach (Ellipse v in connectedValves)
                 {
-                    bool uiAccess = textBlock.Dispatcher.CheckAccess();
-                    if (uiAccess)
-                        textBlock.Text = getTankInfo(textBlock.Name);
+                    bool uiAccess = v.Dispatcher.CheckAccess();
+                    if (uiAccess)// as it is a async task it needs to check permissions
+                    {
+                        string name = v.Uid;
+                        TankModule target = tankList.Find(x => x.Name == name.Split('_')[0]);
+                        TankModule source = target.InFlowTanks.Find(x => x.Name == name.Split('_')[1]);
+                        if (source != null)
+                        {
+                            if(source.OutValveOpen && target.InletFlow > 0)
+                            {
+                                v.Fill = Brushes.White;
+                            }
+                            else
+                            {
+                                v.Fill = Brushes.Black;
+                            }
+                        }
+
+                    }
                     else
-                        textBlock.Dispatcher.Invoke(() => { textBlock.Text = getTankInfo(textBlock.Name); });
+                    {
+                        v.Dispatcher.Invoke(() => {
+                            string name = v.Uid;
+                            TankModule target = tankList.Find(x => x.Name == name.Split('_')[0]);
+                            TankModule source = target.InFlowTanks.Find(x => x.Name == name.Split('_')[1]);
+                            if (source != null)
+                            {
+                                if (source.OutValveOpen && target.InletFlow > 0)
+                                {
+                                    v.Fill = Brushes.White;
+                                }
+                                else
+                                {
+                                    v.Fill = Brushes.Black;
+                                }
+                            }
+                        });
+                    }
                 }
+                //foreach (TextBlock textBlock in textBlocks)
+                //{
+                //    bool uiAccess = textBlock.Dispatcher.CheckAccess();
+                //    if (uiAccess)
+                //        textBlock.Text = getTankInfo(textBlock.Name);
+                //    else
+                //        textBlock.Dispatcher.Invoke(() => { textBlock.Text = getTankInfo(textBlock.Name); });
+                //}
                 await Task.Delay(1000);
             }
         }
@@ -135,8 +257,8 @@ namespace SimulatorUI
                 }
                 msg += "\n";
             }
-            msg += "InFlow: " + Math.Round(tank.InletFlow, 3) + "K\n";
-            msg += "InFow Temp: " + Math.Round(tank.InFlowTemp, 3) + "m3/s\n";
+            msg += "InFlow: " + Math.Round(tank.InletFlow, 3) + "m3/s\n"; 
+            msg += "InFow Temp: " + Math.Round(tank.InFlowTemp, 3) + "K\n";
             msg += "OutFlow: " + Math.Round(tank.OutLetFlow, 3) + "K\n";
             msg += "OutFlw Temp: " + Math.Round(tank.OutFlowTemp, 3) + "K\n";
             msg += tank.Name + " Dmp. Valve: " + tank.DumpValveOpen + "\n";
