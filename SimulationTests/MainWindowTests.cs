002123396fea62d2914cc78c84fc64ed5c24f8bd
vaarss <vaarss@hotmail.com>
2021-12-11 19:20:47 +0100

    Tried to fix my test, but it is still failing. Think there are some issues with using tankList because this needs to be initialized, but this involves using alot of the logic in Main. Dont know if that is good practice.

--- SimulationTests/MainWindowTests.cs
+++ SimulationTests/MainWindowTests.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Windows.Controls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Windows;
+using NUnit.Framework;
 
 namespace SimulationTests
 {
-    [TestClass()]
+    [TestClass, RequiresSTA]
     public class MainWindowTests
     {
 
@@ -19,19 +20,28 @@ namespace SimulationTests
         List<TankModule> tankList = new List<TankModule>();
 
         [TestMethod]
+        public void SwitchViewTest()
+        {
         /*This test is currently just checking if the currentPage i different 
         before and after running switchView. Should probably be made a little
         more advanced. */
-        public void SwitchViewTest()
-        {
-            MainWindow mainWindow = new MainWindow(tankList); 
-            //denne sender jo bare innen tom liste
 
-            expectedCurr = mainWindow.currentPage;
-            mainWindow.SwitchView(sender, e);
-            result = mainWindow.currentPage;
+            Application.Current.Dispatcher.Invoke(delegate
+            {
+                MainWindow mainWindow = new MainWindow(tankList)
+                {
+                    currentPage = new SimulationPage(tankList)
+                };
+                //denne sender jo bare innen tom liste, får nullreferene exception
+                //assuming tanklist cant be empty, but it is set in Main and it feels
+                // wrong to use Main in this testclass as well.
 
-            Assert.AreNotEqual(expectedCurr, result);
+                expectedCurr = mainWindow.currentPage;
+                mainWindow.SwitchView(sender, e);
+                result = mainWindow.currentPage;
+
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(expectedCurr, result);
+            });
         }
     }
 }
