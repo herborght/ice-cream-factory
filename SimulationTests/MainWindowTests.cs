458b8fce68653296a1ddfa74d35109186b001540
Bendik Haugen <bendik_haugen@hotmail.com>
2021-12-12 22:46:14 +0100

    added testing for TankModule constructor and setters

--- SimulationTests/MainWindowTests.cs
+++ SimulationTests/MainWindowTests.cs
@@ -43,5 +43,38 @@ namespace SimulationTests
                 Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(expectedCurr, result);
             });
         }
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            string name = "T1";
+            TankModule T1 = new TankModule("T1");
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(name, T1.Name);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(0, T1.InFlowTanks.Count);
+        }
+        TankModule T1 = new TankModule("T1");
+        TankModule T2 = new TankModule("T2");
+        [TestMethod]
+        public void SettersTest()
+        {
+            T1.Height = 2;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(2, T1.Height, "Height setter does not work properly");
+            T1.DumpValveOpen = false;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(T1.DumpValveOpen, false);
+            T1.InFlowTemp = 277;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(T1.InFlowTemp, 277);
+            T1.Level = 1.5;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(T1.Level, 1.5);
+            double pres = 1.5 / 2;
+            T1.LevelPercentage = pres;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(T1.LevelPercentage, pres);
+            T1.InletFlow = 0.0075;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(T1.InletFlow, 0.0075);
+            T1.OutFlowTemp = 277;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(T1.OutFlowTemp, 277);
+            T1.OutValveOpen = false;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(T1.OutValveOpen, false);
+            T1.Temperature = 277;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(T1.Temperature, 277);
+        }
     }
 }
