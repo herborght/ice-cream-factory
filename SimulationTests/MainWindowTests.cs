ef24cc5abb3f461cd9e90083d394f630d5837e6a
vaarss <vaarss@hotmail.com>
2021-12-13 01:37:28 +0100

    Added test to the displayModule class

--- SimulationTests/MainWindowTests.cs
+++ SimulationTests/MainWindowTests.cs
@@ -6,26 +6,43 @@ using System.Windows.Controls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Windows;
 using NUnit.Framework;
+using ABB.InSecTT.Common.Configuration;
 
 namespace SimulationTests
 {
     [TestClass, RequiresSTA]
     public class MainWindowTests
     {
+        //initalizing main
+        IParameterDataBase parameters;
+        IEnumerable<IModule> modules; 
+        string configFilePath;
+        //Main main = new Main(parameters, modules, configFilePath);
 
+        //variables for SwitchViewTest
         Page expectedCurr;
         Page result;
         object sender;
         RoutedEventArgs e;
+
+        //variables for MainWIndow (but think i can just use main.)
+        TankModule T1;
+        TankModule T2;
         List<TankModule> tankList = new List<TankModule>();
+
 
 
         [TestMethod]
         public void SwitchViewTest()
         {
-        /*This test is currently just checking if the currentPage i different 
-        before and after running switchView. Should probably be made a little
-        more advanced. */
+            /*This test is currently just checking if the currentPage i different 
+            before and after running switchView. Should probably be made a little
+            more advanced. */
+
+            T1 = new TankModule("T1");
+            T2 = new TankModule("T2");
+            tankList.Add(T1); //need to run initialize tanks to create tankList with the config path 
+            tankList.Add(T2);
 
             Application.Current.Dispatcher.Invoke(delegate
             {
@@ -45,10 +62,15 @@ namespace SimulationTests
             });
         }
 
-        //se i getTankInfo
         [TestMethod]
-        public void GetInfoTest()
+        public void GetTnkInfoTest()
         {
+            T1 = new TankModule("T1");
+            T2 = new TankModule("T2");
+            
+            tankList.Add(T1);
+            tankList.Add(T2);
+
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(1, 1);
 
         }
